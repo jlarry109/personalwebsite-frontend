798becc431d2c3744f4801b1017f332b002a2946
Jones Larry <jones.larry@duke.edu>
2025-03-27 13:14:44 -0400

    server.mjs -> server.cjs

--- server.cjs
+++ server.cjs
@@ -1,7 +1,12 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import express from 'express';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+
+const express = require('express');
+const path = require('path');
+const { fileURLToPath } = require('url');
+const { dirname } = require('path');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
