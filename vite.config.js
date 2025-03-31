19f6ecf04f0ae0c6639f373668c71eda11345e14
Jones Larry <jones.larry@duke.edu>
2025-03-30 22:44:56 -0400

    change favicon

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,3 @@
-// import { fileURLToPath } from 'node:url';
-// import { dirname } from 'node:path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-// import vueDevTools from 'vite-plugin-vue-devtools';
-
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "http://me-api.joneslarry.com",
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-//   plugins: [vue(), vueDevTools()],
-//   resolve: {
-//       alias: {
-//         '@': fileURLToPath(new URL('./src', import.meta.url)) // Should point to 'src'
-//       }
-//     }
-// });
-
-
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 import { defineConfig } from 'vite';
