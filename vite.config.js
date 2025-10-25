afe725b1ef38492aaf653a86243a90d1607a290d
JL <atpghs@amazon.com>
2025-10-24 23:41:36 -0700

    Configure custom domain

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/personalwebsite-frontend/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
